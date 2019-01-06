6415947ca2d3d2d146b6751676fcc079174b723d
Reese Long <reese.long@acuitybrands.com>
2019-01-06 12:33:13 -0500

    initial commit

---
+++ single_image_analysis.py
@@ -0,0 +1,25 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from distutils.version import StrictVersion
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+from object_detection.utils import ops as utils_ops
+
+if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
+  raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
+# This is needed to display the images.
+%matplotlib inline
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
